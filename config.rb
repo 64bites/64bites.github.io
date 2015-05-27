ed19fa9a6b3b5f57796809de931fe307a90a35c6
Michał Taszycki <michal@gunpowderlabs.com>
2015-05-27 03:19:36 +0200

    Add open graph info

--- config.rb
+++ config.rb
@@ -11,6 +11,10 @@ set :images_dir, 'assets/images'
 set :fonts_dir, 'assets/fonts'
 set :layout, 'layouts/application'
 
+set :protocol, "http://"
+set :host, "64bites.com"
+set :port, 80
+
 configure :development do
  activate :livereload
 end
@@ -45,19 +49,45 @@ helpers do
   end
 
   def episode_path(episode)
-    "/episodes/#{episode.slug}.html"
+    "/episodes/#{episode.slug}"
   end
 
   def featured_episodes
     FEATURED_EPISODES
   end
+  def host_with_port
+    [host, optional_port].compact.join(':')
+  end
+
+  def optional_port
+    port unless port.to_i == 80
+  end
+
+  def image_url(source)
+    page_url(image_path(source))
+  end
+
+  def episode_url(episode)
+    page_url(episode_path(episode))
+  end
+
+  def page_url(page_path)
+    protocol + host_with_port + page_path
+  end
+
+end
+
+configure :development do
+  # Used for generating absolute URLs
+  set :host, "64bites-test.ngrok.com" #Middleman::PreviewServer.host
+  set :port, "80" #Middleman::PreviewServer.port
 end
 
 
 CATALOG.all_episodes.each do |episode|
   template = "/views/templates/episodes/show-free.html"
   puts episode_path(episode)
-  proxy episode_path(episode), template, :locals => { :episode => episode }
+  proxy episode_path(episode) + ".html", template, :locals => { :episode => episode }, layout: 'episode'
 end
 
 
