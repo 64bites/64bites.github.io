1884d55036fc7f228e28de3601176ce093733b0f
Michał Taszycki <michal@gunpowderlabs.com>
2015-06-13 14:14:26 +0200

    fix episodes partials

--- config.rb
+++ config.rb
@@ -123,7 +123,7 @@ end
 
 CATALOG.all_episodes.each do |episode|
   template = episode.free? ? "/views/templates/episodes/show-free.html" : "/views/templates/episodes/show-paid.html"
-  proxy episode_path(episode) + ".html", template, :locals => { :episode => episode }, layout: 'episode'
+  proxy episode_path(episode) + ".html", template, :locals => { :episode => episode }, layout: 'episode', ignore: true
 end
 
 
