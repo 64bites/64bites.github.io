c6c5d3efe6294525944869abcc4e1e29b9a17706
Michał Taszycki <mtaszycki@gmail.com>
2017-11-27 11:56:39 +0100

    Cybermonday code
    Cybermonday code
    
    Update season#001's poster
    
    Optimize season#001's poster
    
    Update normalize
    
    Move base stylesheets to vendor
    
    Move font-awesome stylesheets to vendor
    
    Move full-width-section to mixins
    
    Get rid of youtube video
    
    Get rid of subscriptions
    
    Get rid of opting-in & out
    
    Get rid of old-price styling
    
    Move episode descriptions
    
    No more wrapper
    
    Extract ribbon component
    
    Extract pricing tiers component
    
    Extract testimonials component
    
    Clean up old shit
    
    Bring back featured episodes.
    
    better styling for featured episodes
    
    better featured episodes
    
    Add season component
    
    Extract episode component
    
    More styling improvements.
    
    Homepage
    
    Add variables
    
    better buttons
    
    Add season 1 to home
    
    ladning copy
    
    More copy
    
    Update season#001's poster
    
    Optimize season#001's poster

--- lib/models/catalog.rb
+++ lib/models/catalog.rb
@@ -19,7 +19,7 @@ class Catalog
   def find_season_by_episode(episode)
     season_number = nil
     season_number = (episode.number - 1).div(EPISODES_PER_SEASON) + 1
-    find_season(season_number)    
+    find_season(season_number)
   end
 
   def all_episodes_in_season(season)
@@ -37,23 +37,23 @@ class Catalog
       all_episodes_in_season(season)
     end
   end
-  
+
   def free_episodes
     all_episodes.select(&:free?)
   end
 
   def featured_episodes
-    free_episodes.select do |episode|
-      featured_episodes_data.include? episode.number
-    end
-  end 
+    featured_episodes_data.map do |episode_number|
+      find_episode(episode_number)
+    end.select(&:free?)
+  end
 
   def latest_episode
     all_episodes.first
   end
 
   def latest_episode?(episode)
-    episode.number == latest_episode.number 
+    episode.number == latest_episode.number
   end
 
   def all_episodes
@@ -102,11 +102,11 @@ class Catalog
       subscription_data["gumroad_product_id"],
     )
   end
-  
+
   def all_seasons
     last_season_number = seasons_data.keys.map { |slug| decode_number(slug) }.max
     seasons_data.map do |season_slug, season_data|
-      number = decode_number(season_slug) 
+      number = decode_number(season_slug)
       season = Season.new(
         number == last_season_number,
         number,
@@ -122,8 +122,8 @@ class Catalog
 
   private
   attr_reader :episodes_data, :featured_episodes_data, :seasons_data, :subscription_data
-  
+
   def decode_number(episode_slug)
-    episode_slug.split("-").first.to_i 
+    episode_slug.split("-").first.to_i
   end
 end
